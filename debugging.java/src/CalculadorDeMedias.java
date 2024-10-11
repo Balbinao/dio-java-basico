05bd7bdad012f14c984a9c13a55846bf8aec3989
Gustavo Balbino <guto9201@gmail.com>
2024-10-10 21:38:55 -0300

    Teste de debugging

---
+++ debugging.java/src/CalculadorDeMedias.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculadorDeMedias{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+        System.out.printf("Média da turma: %.1f", media);
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner){
+        double soma = 0;
+        for(String aluno : alunos){
+            System.out.println("Nota do aluno:" + aluno);
+            double nota = scanner.nextInt();
+            soma+=nota;
+
+        }
+        return soma/alunos.length;
+    }
+}
